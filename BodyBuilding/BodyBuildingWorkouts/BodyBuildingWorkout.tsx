8a0890041e129653996ee725e9d936a40a8f5a15
Jafaar Haddad <144615906+JafaarH@users.noreply.github.com>
2023-10-19 17:25:39 +0300

    Update BodyBuildingWorkout.tsx

--- BodyBuilding/BodyBuildingWorkouts/BodyBuildingWorkout.tsx
+++ BodyBuilding/BodyBuildingWorkouts/BodyBuildingWorkout.tsx
@@ -7,66 +7,14 @@ import { trainingData } from "./trainningData";
 import Image, { StaticImageData } from "next/image";
 import RatingTrain from "./RatingTrain";
 import "./BodyBuildingWorkoutStyle.css";
+import BDNavbar from "../BodyBuildingPage/BDNavbar.tsx"
 
 const BDWorkout = () => {
     const {dbCartProduct, showLegTrain, showAbsTrain ,showedTrain, showAllTrain, showBicepsTrain,showChestTrain} = useContext(Shop)
-    const [show, setShow] = useState(false)
-  const handleShow = () => setShow(true);
-  const handleClose = () => setShow(false)
+    
     return (
         <div>
-            <Navbar bg="dark" data-bs-theme="dark" className="dbnavbar" >
-        <Container fluid>
-            <Link to="/BodyBuilding" className="textDeco">
-          <Navbar.Brand>Haddad's Gym</Navbar.Brand>
-          </Link>
-          <Nav className="m-auto">
-            <Link to="/BodyBuilding/Shop" className="textDeco">
-            <Nav className="navPage2" >Shop</Nav>
-            </Link>
-            <Link to="/BodyBuilding/Coaches" className="textDeco">
-            <Nav className="navPage" >Meet your Coach</Nav>
-            </Link>
-            <Link to="/BodyBuilding/Workout" className="textDeco">
-            <Nav className="navPage" >Workout Plans</Nav>
-            </Link>
-          </Nav>
-          <Link to="/">
-            <button className="changedButton">Changed my mind</button>
-            </Link>
-            <Link to="/BodyBuilding/Cart">
-            <BiShoppingBag className="shoppingBag" />
-            </Link>
-            <Badge bg="dark" className="badge">{dbCartProduct.length}</Badge>
-            <div className="disappearTabletSize">
-            <Button variant="danger" onClick={handleShow} className="me-2">
-        Nav
-      </Button>
-      <Offcanvas placement="end" data-bs-theme="dark" show={show} onHide={handleClose} >
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title>Navigation Side</Offcanvas.Title>
-        </Offcanvas.Header>
-        <Offcanvas.Body>
-        <Link to="/BodyBuilding/Shop" className="textDeco">
-            <Nav className="pageNavSide">Shop Page</Nav>
-            </Link>
-            <Link to="/BodyBuilding/Coaches" className="textDeco">
-            <Nav className="pageNavSide">Meet your Coach Page</Nav>
-            </Link>
-            <Link to="/BodyBuilding/Workout" className="textDeco">
-            <Nav className="pageNavSide">Workout Plans Page</Nav>
-            </Link>
-            <Link to="/BodyBuilding/Cart" className="textDeco">
-              <Nav className="pageNavSide">Cart Page</Nav>
-            </Link>
-            <Link to="/">
-              <button className="buttonNavSide">Changed my Mind</button>
-            </Link>
-        </Offcanvas.Body>
-      </Offcanvas>
-            </div>
-    </Container>
-    </Navbar>
+           <BDNavbar />
         <div className="sortingNavbar">
             <button onClick={() => showAllTrain(showedTrain)} className="sortingButton">All</button>
             <button onClick={() => showBicepsTrain(showedTrain)} className="sortingButton">Biceps</button>
