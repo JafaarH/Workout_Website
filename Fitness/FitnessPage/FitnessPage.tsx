93d8ad3857d8bb8b3283cad33f685309c7da406d
Jafaar Haddad <144615906+JafaarH@users.noreply.github.com>
2023-10-19 17:14:00 +0300

    Update FitnessPage.tsx

--- Fitness/FitnessPage/FitnessPage.tsx
+++ Fitness/FitnessPage/FitnessPage.tsx
@@ -6,6 +6,7 @@ import Image from "next/image"
 import running from "../../images/running.jpg"
 import { Shop } from "@/app/Context and Reducer Ftiness/Context";
 import { useContext, useState } from "react";
+import FitnessNavbar from "./FitnessNavbar.tsx"
 
 const FitnessPage =() => {
   const {cartProducts} = useContext(Shop)
@@ -14,58 +15,7 @@ const FitnessPage =() => {
   const handleClose = () => setShow(false)
     return (
         <div>
-            <Navbar bg="primary" data-bs-theme="dark" className="navbar">
-        <Container fluid>
-            <Link to="/Fitness" className="textDeco">
-          <Navbar.Brand>Haddad's Gym</Navbar.Brand>
-          </Link>
-          <Nav className="m-auto">
-            <Link to="/Fitness/Shop" className="textDeco">
-            <Nav className="piece2">Shop</Nav>
-            </Link>
-            <Link to="/Fitness/Coaches" className="textDeco">
-            <Nav className="piece">Meet your coach</Nav>
-            </Link>
-            <Link to="/Fitness/Workout" className="textDeco">
-            <Nav className="piece" >Workout plans</Nav>
-            </Link>
-          </Nav>
-          <Link to="/" className="textDeco">
-          <button className="changeBttn">Changed my mind</button>
-          </Link>
-          <Link to="/Fitness/Cart" className="textDeco">
-          <BiShoppingBag className="shoppingTag" />
-          </Link>
-          <Badge>{cartProducts.length}</Badge>
-          <div className="disappearTabletSize">
-            <Button variant="success" onClick={handleShow} className="me-2">
-        Nav
-      </Button>
-      <Offcanvas placement="end" data-bs-theme="dark" show={show} onHide={handleClose} >
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title>Navigation Side</Offcanvas.Title>
-        </Offcanvas.Header>
-        <Offcanvas.Body>
-        <Link to="/Fitness/Shop" className="textDeco">
-            <Nav className="pageNavSide">Shop Page</Nav>
-            </Link>
-            <Link to="/Fitness/Coaches" className="textDeco">
-            <Nav className="pageNavSide">Meet your Coach Page</Nav>
-            </Link>
-            <Link to="/Fitness/Workout" className="textDeco">
-            <Nav className="pageNavSide">Workout Plans Page</Nav>
-            </Link>
-            <Link to="/Fitness/Cart" className="textDeco">
-              <Nav className="pageNavSide">Cart Page</Nav>
-            </Link>
-            <Link to="/">
-              <button className="buttonNavSide">Changed my Mind</button>
-            </Link>
-        </Offcanvas.Body>
-      </Offcanvas>
-            </div>
-        </Container>
-      </Navbar>
+           <FitnessNavbar />
       <div className="peopleRunning">
         <Image src={running} alt="people running" className="ImgPeopleRunning" />
         <div className="welcome">
