817a455e51de5bf14f426ece41d6634ad0bcab25
Jafaar Haddad <144615906+JafaarH@users.noreply.github.com>
2023-10-19 17:17:41 +0300

    Update Workout.tsx

--- Fitness/Workout/Workout.tsx
+++ Fitness/Workout/Workout.tsx
@@ -8,6 +8,7 @@ import { dataWorkout } from "./dataWorkout";
 import Image from "next/image";
 import "./WorkoutStyle.css";
 import Rating from "./RatingSystem";
+import FitnessNavbar from "../FitnessPage/FitnessNavbar.tsx"
 
 const Workout = () => {
     const {cartProducts} = useContext(Shop)
@@ -16,58 +17,7 @@ const Workout = () => {
   const handleClose = () => setShow(false)
     return (
         <div>
-            <Navbar bg="primary" data-bs-theme="dark" className="navbar">
-                <Container fluid>
-            <Link to="/Fitness" className="textDeco">
-          <Navbar.Brand>Haddad's Gym</Navbar.Brand>
-          </Link>
-          <Nav className="m-auto" >
-            <Link to="/Fitness/Shop" className="textDeco">
-            <Nav className="piece2">Shop</Nav>
-            </Link>
-            <Link to="/Fitness/Coaches" className="textDeco">
-            <Nav className="piece">Meet your coach</Nav>
-            </Link>
-            <Link to="/Fitness/Workout" className="textDeco">
-            <Nav className="piece" >Workout plans</Nav>
-            </Link>
-          </Nav>
-          <Link to="/" className="textDeco">
-          <button className="changeBttn">Changed my mind</button>
-          </Link>
-          <Link to="/Fitness/Cart" className="textDeco">
-          <BiShoppingBag className="shoppingTag" />
-          </Link>
-          <Badge>{cartProducts.length}</Badge>
-          <div className="disappearTabletSize">
-            <Button variant="success" onClick={handleShow} className="me-2">
-        Nav
-      </Button>
-      <Offcanvas placement="end" data-bs-theme="dark" show={show} onHide={handleClose} >
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title>Navigation Side</Offcanvas.Title>
-        </Offcanvas.Header>
-        <Offcanvas.Body>
-        <Link to="/Fitness/Shop" className="textDeco">
-            <Nav className="pageNavSide">Shop Page</Nav>
-            </Link>
-            <Link to="/Fitness/Coaches" className="textDeco">
-            <Nav className="pageNavSide">Meet your Coach Page</Nav>
-            </Link>
-            <Link to="/Fitness/Workout" className="textDeco">
-            <Nav className="pageNavSide">Workout Plans Page</Nav>
-            </Link>
-            <Link to="/Fitness/Cart" className="textDeco">
-              <Nav className="pageNavSide">Cart Page</Nav>
-            </Link>
-            <Link to="/">
-              <button className="buttonNavSide">Changed my Mind</button>
-            </Link>
-        </Offcanvas.Body>
-      </Offcanvas>
-            </div>
-        </Container>
-      </Navbar>
+            <FitnessNavbar />
       {
         dataWorkout.map((move, i)=> (
             <div key={i} className="workout" >
