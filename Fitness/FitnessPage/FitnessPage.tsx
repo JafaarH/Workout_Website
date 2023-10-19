0f6c1b165d3fdba0c1547e05d81c8616250da612
Jafaar Haddad <144615906+JafaarH@users.noreply.github.com>
2023-10-19 17:27:31 +0300

    Update FitnessPage.tsx

--- Fitness/FitnessPage/FitnessPage.tsx
+++ Fitness/FitnessPage/FitnessPage.tsx
@@ -10,9 +10,7 @@ import FitnessNavbar from "./FitnessNavbar.tsx"
 
 const FitnessPage =() => {
   const {cartProducts} = useContext(Shop)
-  const [show, setShow] = useState(false)
-  const handleShow = () => setShow(true);
-  const handleClose = () => setShow(false)
+
     return (
         <div>
            <FitnessNavbar />
