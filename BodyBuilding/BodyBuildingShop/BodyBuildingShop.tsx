3972e849266bbda22dae2a2306161e7ef75045b7
Jafaar Haddad <144615906+JafaarH@users.noreply.github.com>
2023-10-19 17:23:15 +0300

    Update BodyBuildingShop.tsx

--- BodyBuilding/BodyBuildingShop/BodyBuildingShop.tsx
+++ BodyBuilding/BodyBuildingShop/BodyBuildingShop.tsx
@@ -6,6 +6,7 @@ import Image from "next/image"
 import "./BodyBuildingStyle.css"
 import { Shop } from "@/app/Context and Reducer Ftiness/Context";
 import { useContext, useState } from "react";
+import BDNavbar from "../BodyBuildingPage/BDNavbar.tsx"
 
 const BodyBuildingShop = () => {
     const {showededProduct, showProduct, dbCartProduct} = useContext(Shop)
@@ -14,58 +15,7 @@ const BodyBuildingShop = () => {
   const handleClose = () => setShow(false)
     return (
         <div className="body">
-            <Navbar bg="dark" data-bs-theme="dark" className="navbar" >
-        <Container fluid>
-            <Link to="/BodyBuilding" className="textDeco">
-          <Navbar.Brand>Haddad's Gym</Navbar.Brand>
-          </Link>
-          <Nav className="m-auto">
-            <Link to="/BodyBuilding/Shop" className="textDeco">
-            <Nav className="navPage2" >Shop</Nav>
-            </Link>
-            <Link to="/BodyBuilding/Coaches" className="textDeco">
-            <Nav className="navPage" >Meet your Coach</Nav>
-            </Link>
-            <Link to="/BodyBuilding/Workout" className="textDeco">
-            <Nav className="navPage" >Workout Plans</Nav>
-            </Link>
-          </Nav>
-          <Link to="/">
-            <button className="changedButton">Changed my mind</button>
-            </Link>
-            <Link to="/BodyBuilding/Cart">
-            <BiShoppingBag className="shoppingBag" />
-            </Link>
-            <Badge bg="dark" className="badge">{dbCartProduct.length}</Badge>
-            <div className="disappearTabletSize">
-            <Button variant="danger" onClick={handleShow} className="me-2">
-        Nav
-      </Button>
-      <Offcanvas placement="end" data-bs-theme="dark" show={show} onHide={handleClose} >
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title>Navigation Side</Offcanvas.Title>
-        </Offcanvas.Header>
-        <Offcanvas.Body>
-        <Link to="/BodyBuilding/Shop" className="textDeco">
-            <Nav className="pageNavSide">Shop Page</Nav>
-            </Link>
-            <Link to="/BodyBuilding/Coaches" className="textDeco">
-            <Nav className="pageNavSide">Meet your Coach Page</Nav>
-            </Link>
-            <Link to="/BodyBuilding/Workout" className="textDeco">
-            <Nav className="pageNavSide">Workout Plans Page</Nav>
-            </Link>
-            <Link to="/BodyBuilding/Cart" className="textDeco">
-              <Nav className="pageNavSide">Cart Page</Nav>
-            </Link>
-            <Link to="/">
-              <button className="buttonNavSide">Changed my Mind</button>
-            </Link>
-        </Offcanvas.Body>
-      </Offcanvas>
-            </div>
-        </Container>
-      </Navbar>
+            <BDNavbar />
       <div>
         <h2 className="bdHeading2">Body Building Shop</h2>
         <p className="bdPara">Here you will find everything you need for <br/>home body building workout</p>
