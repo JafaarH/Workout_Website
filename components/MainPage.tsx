2f2942dd34d53ef766b76f801933b0a0e39da12d
Jafaar Haddad <144615906+JafaarH@users.noreply.github.com>
2023-10-20 21:21:21 +0300

    Update MainPage.tsx

--- components/MainPage.tsx
+++ components/MainPage.tsx
@@ -7,17 +7,23 @@ import { Link } from "react-router-dom"
 const  MainPage =() => {
     return (
         <div className="theBigContainer">
+            <header className="containerOfHeading">
             <h1 className="mainHeading">choose your goal</h1>
+            </header>
             <Link to="/Fitness" >
             <div className="containerOfImg1">
                 <Image src={fitness} alt="Fitness" className="mainImg1" />
+                <article className="containerOfSecHeading">
                 <h4 className="mainHeading41">Fitness</h4>
+                </article>
             </div>
             </Link>
             <Link to="/BodyBuilding">
             <div className="containerOfImg2">
                 <Image src={download} alt="Body Building" className="mainImg2" />
+                <article className="containerOfThrHeading">
                 <h4 className="mainHeading42">Body<br /> Building</h4>
+                </article>
             </div>
             </Link>
         </div>
